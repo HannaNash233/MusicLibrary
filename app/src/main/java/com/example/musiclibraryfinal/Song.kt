14790035469ca92b89ae73c574a7890007776433
rolandpelzel <rolandpelzel@go.rmc.edu>
2024-12-04 11:11:34 -0500

    SongsAdapter file

--- app/src/main/java/com/example/musiclibraryfinal/Song.kt
+++ app/src/main/java/com/example/musiclibraryfinal/Song.kt
@@ -2,5 +2,6 @@ package com.example.musiclibraryfinal
 
 data class Song(val title: String = "",
                 val artist: String = "",
-                val genre: String = "") {
+                val genre: String = "",
+                val pubYear: Int) {
 }
