262060c961e9cc9eed0a897a4ca6a6b472f338db
rolandpelzel <rolandpelzel@go.rmc.edu>
2024-12-06 10:58:21 -0500

    SongsAdapter file

--- app/src/main/java/com/example/musiclibraryfinal/connectedActivity.kt
+++ app/src/main/java/com/example/musiclibraryfinal/connectedActivity.kt
@@ -1,18 +1,32 @@
 package com.example.musiclibraryfinal
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.findNavController
 
-class connectedActivity : AppCompatActivity() {
+class ConnectedActivity : AppCompatActivity() {
+    //private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var addBtn: Button
+    private lateinit var deleteBtn: Button
+    private lateinit var songTitleEdit: EditText
+    private lateinit var artistEdit: EditText
+    private lateinit var genreEdit: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,18 +37,50 @@ class connectedActivity : AppCompatActivity() {
             insets
         }
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        //val toolbar: Toolbar = findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
+        //auth = FirebaseAuth.getInstance()
+        // may need to change the code if we use the fragments
+        database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("playlist")
+        addBtn = findViewById<Button>(R.id.addBTN)
+        songTitleEdit = findViewById<EditText>(R.id.songTitleEdit)
+        artistEdit = findViewById<EditText>(R.id.artistEdit)
+        genreEdit = findViewById<EditText>(R.id.genreEdit)
 
-        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+
+
+        addBtn.setOnClickListener {
+            val songTitle = songTitleEdit.text.toString()
+            val artistName = artistEdit.text.toString()
+            val genre = genreEdit.text.toString()
+
+            if(songTitle.isEmpty() || artistName.isEmpty() || genre.isEmpty()){
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val song = Song(songTitle, artistName, genre)
+
+                ref.child(songTitle).setValue(song).addOnSuccessListener {
+                    Toast.makeText(this, "Song saved in playlist!", Toast.LENGTH_SHORT).show()
+                }
+                    .addOnFailureListener{
+                        Toast.makeText(this, "Failed to save data", Toast.LENGTH_SHORT).show()
+                    }
+            }
+           // add fragment code
+        }
+
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    // i commented the tool bar but didn't delete completely in case we want to do it again - Hanna
+    // also please look at the connected.xml and if it isn't the vision go for the change
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.mainmenu, menu)
         return true
-    }
+    }*/
 
-    override fun onOptionsItemSelected(item: MenuItem) : Boolean {
+    /*override fun onOptionsItemSelected(item: MenuItem) : Boolean {
         when (item.itemId) {
             R.id.menuMain -> Toast.makeText(this, "Main Menu clicked", Toast.LENGTH_SHORT).show()
             R.id.menuAdd -> Toast.makeText(this, "Add Song clicked", Toast.LENGTH_SHORT).show()
@@ -42,10 +88,5 @@ class connectedActivity : AppCompatActivity() {
             R.id.menuLogout -> Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show()
         }
         return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp()||super.onSupportNavigateUp()
-    }
+    }*/
 }
