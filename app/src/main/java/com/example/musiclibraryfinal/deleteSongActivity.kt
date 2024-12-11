1f84c1768404791fa1067bbb48e30e59465e919c
rolandpelzel <rolandpelzel@go.rmc.edu>
2024-12-11 18:21:52 -0500

    This returns true

--- app/src/main/java/com/example/musiclibraryfinal/deleteSongActivity.kt
+++ app/src/main/java/com/example/musiclibraryfinal/deleteSongActivity.kt
@@ -80,6 +80,6 @@ class deleteSongActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-        return true // This returns true
+        return true
     }
 }
