79db255d8e44c1959a52c4d50e7d40eb683a78f6
Carson <carsontriplett@go.rmc.edu>
2024-12-11 19:07:17 -0500

    Implemented the searchSongActivity

--- app/src/main/java/com/example/musiclibraryfinal/searchSongActivity.kt
+++ app/src/main/java/com/example/musiclibraryfinal/searchSongActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.database.FirebaseDatabase
 
 class searchSongActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,48 @@ class searchSongActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val btnsearch = findViewById<Button>(R.id.btn_search)
+        val et_title_search = findViewById<EditText>(R.id.et_title_search)
+        val tv_dataTitle = findViewById<TextView>(R.id.tv_dataTitle)
+        val tv_dataArtist = findViewById<TextView>(R.id.tv_dataArtist)
+        val tv_dataGenre = findViewById<TextView>(R.id.tv_dataGenre)
+        val tv_dataYear = findViewById<TextView>(R.id.tv_dataYear)
+
+
+        btnsearch.setOnClickListener {
+            val songName = et_title_search.text.toString()
+            if (songName.isEmpty()) {
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("Songs")
+                myRef.child(songName).get().addOnSuccessListener { snapshot ->
+                    if (snapshot.exists()) {
+                        val song = snapshot.getValue(Song::class.java)
+                        if (song != null) {
+                            Toast.makeText(
+                                this,
+                                "Title: ${song.title}, Artist: ${song.artist}, Genre: ${song.genre}, Year: ${song.pubYear}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            tv_dataTitle.text = song.title
+                            tv_dataArtist.text = song.artist
+                            tv_dataGenre.text = song.genre
+                            tv_dataYear.text = song.pubYear.toString()
+                        }
+                        } else {
+                            Toast.makeText(this, "no such song", Toast.LENGTH_SHORT).show()
+                        }
+                    }.addOnFailureListener {
+                        Toast.makeText(this, "Error reading data", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         Log.d("Debug", "In the menu")
